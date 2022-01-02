61a0e43a126577d809225144b65aa5365e29bf1b
Artur <info@aigroupusa.com>
2022-01-02 14:20:42 -0500

    commit tree bea84a4583ea9d042bd340a0b8b63d7829fc74f7

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    let record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+   return record
+}
 
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord)
+ }
+
+ function dateStampObj(type, dateStamp) {
+    return {type: type, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+} 
+
+function createTimeInEvent(date_stamp) {
+    this.timeInEvents.push(dateStampObj("TimeIn", date_stamp))
+    return this
+}
+
+function createTimeOutEvent(date_stamp) {
+    this.timeOutEvents.push(dateStampObj("TimeOut", date_stamp))
+    return this
+}
+
+function hoursWorkedOnDate(form_date) { 
+    const timeIn = this.timeInEvents.find((e) => e.date === form_date).hour
+    const timeOut = this.timeOutEvents.find((e) => e.date === form_date).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(form_date) {
+    const payOwed = hoursWorkedOnDate.call(this, form_date) * this.payPerHour
+    return payOwed
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +58,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((record) => record.firstName === firstName)
+}
+
+function calculatePayroll(array) {
+    const allPayOwed = (array.map((em) => { return allWagesFor.call(em)}))
+    return allPayOwed.reduce((accum, curVal) => accum + curVal)
 }
